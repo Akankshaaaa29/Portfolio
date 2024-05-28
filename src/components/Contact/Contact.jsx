0d12b78f284a992a6f170ac78fb22f31f4f7f147
Akanksha Jha <99268258+Akankshaaaa29@users.noreply.github.com>
2024-05-28 20:02:41 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:akankshajha290521@email.com">akankshajha290521@email.com</a>
+          <a href="mailto:akankshajha290521@gmail.com">akankshajha290521@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
